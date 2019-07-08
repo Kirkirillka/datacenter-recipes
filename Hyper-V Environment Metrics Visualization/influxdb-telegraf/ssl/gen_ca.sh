27bfc6f59e2a0bfdfb6ba9e448cc29cac8be0cee
Kirill Kukarcev <kirillkukarcev@gmail.com>
2019-07-08 10:18:45 +0300

    Hyper-V. Add access to Grafana through nginx container with TLS.

--- Hyper-V Environment Metrics Visualization/influxdb-telegraf/ssl/gen_ca.sh
+++ Hyper-V Environment Metrics Visualization/influxdb-telegraf/ssl/gen_ca.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
 
+# Generate for nginx
+openssl req \
+  -new \
+  -newkey rsa:4096 \
+  -days 3650 \
+  -nodes \
+  -x509 \
+  -subj "/C=RU/ST=RU/L=Kazan/O=nginx ingress/CN=nginx.dc.local" \
+  -keyout nginx.key \
+  -out nginx.pem
+
+
+# Generate for InfluxDB
 openssl req \
   -new \
   -newkey rsa:4096 \
