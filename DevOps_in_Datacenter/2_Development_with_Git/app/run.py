81965b64de59866b3289ca09fb8e2413a4f25a48
Kir <kirillkukarcev@gmail.com>
2020-04-11 14:30:52 +0300

    Comments on basic URIs

--- DevOps_in_Datacenter/2_Development_with_Git/app/run.py
+++ DevOps_in_Datacenter/2_Development_with_Git/app/run.py
@@ -6,6 +6,10 @@ app = flask.Flask(__name__)
 
 @app.route("/")
 def index():
+    """
+        Initial page to return on "/" path
+    :return:
+    """
     return jsonify({
         "status": "ok",
         "page": "index"
@@ -14,12 +18,15 @@ def index():
 
 @app.route("/user/get/<int:id>",methods=["GET"])
 def get_by_id(id):
+    """
+        URI to return data about the user for the given ID
+    :param id: user_id
+    :return:
+    """
     return jsonify({
         "status": "ok",
         "requested_id": id
     })
-
-
 
 if __name__ == '__main__':
     app.run("0.0.0.0",9000)
